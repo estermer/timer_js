09244a1bbb93c73868226b3a8c92c005b63f8454
estermer <eric.stermer@gmail.com>
2016-11-29 09:42:48 -0500

    create start tiimer function tha changed the h1 tag as time elapsed

--- timers.js
+++ timers.js
@@ -1,4 +1,38 @@
+window.onload = function(){
+
+  let timeElapsed = 0;
+  let startButton = document.getElementById('start');
+  let resetButton = document.getElementById('reset');
+  let pauseButton = document.getElementById('pause');
+  let timerText = document.getElementById('timer');
+
+
+  let startTimer = () => {
+
+    let intervalID = window.setInterval(function(){
+      timerText.innerHTML = "Time elapsed: " + timeElapsed;
+      console.log(timeElapsed);
+      timeElapsed++;
+    }, 1000);
+  };
+
+  startButton.addEventListener('click', startTimer);
+
+
+  //When "Reset" is clicked, the text should return to "Stop Watch"
+
+    //grab the reset button and set to variable
+    // set text from "time elapsed" to "stop Watch"
+    //clear interval
+
+
+  //When "Pause" is clicked, the text should
+  //say "Time elapsed: 1", but stop incrementing.
+    //clear interval
 
 
 
 
+
+
+};
